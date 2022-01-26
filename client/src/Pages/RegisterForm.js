6e28ff35ae11f433c3cc96b92ac4ea8e3e9b16ee
Sagar Raj Thapaliya <sr3959596@gmail.com>
2022-01-26 12:34:57 +0545

    added register form page for register validation

---
+++ client/src/Pages/RegisterForm.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+import { useState } from "react";
+import "./RegisterForm.css";
+
+
+const Label = styled.label`
+    font-size: 12px;
+    color: gray;
+`;
+
+const RegisterForm = (props) => {
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
+
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
+
+  return (
+    <div>
+      <Label>{label}</Label>
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        onFocus={() =>
+          inputProps.name === "confirmPassword" && setFocused(true)
+        }
+        focused={focused.toString()}
+      />
+      <span>{errorMessage}</span>
+    </div>
+  );
+};
+
+export default RegisterForm;
+
+

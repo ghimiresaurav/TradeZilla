6127aab24a7abcdac5afae2f335c9df052c295b0
Saurav Ghimire <ghimiresaurav09@gmail.com>
2022-02-08 16:27:04 +0545

    typescript init

---
+++ server/src/controllers/register.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+
+const register = (req: Request, res: Response) => {
+  console.log("register request");
+  console.log("asdfkolasdjf");
+};
+
+export default register;
---
+++ server/src/index.ts
@@ -0,0 +1,22 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+//import controllers
+import register from "./controllers/register";
+
+const app: Application = express();
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  return res.json({ message: "Hello" });
+});
+
+app.post("/register", register);
+
+const PORT: number = parseInt(<string>process.env.PORT) || 5000;
+
+app.listen(PORT, () => console.log(`SERVER RUNNING ON PORT ${PORT}`));

6be515e44480c845d681c263bd5c8b8ad2523dc4
Saurav Ghimire <ghimiresaurav09@gmail.com>
2022-02-12 15:57:00 +0545

    create product schema

---
+++ server/src/models/Product.ts
@@ -0,0 +1,36 @@
+import { Schema, model, Types } from "mongoose";
+
+interface ProductType {
+  title: string;
+  vendor: Types.ObjectId;
+  postedOn: Date;
+  rating: Number;
+  mfgDate: Date;
+}
+
+const ProductSchema = new Schema<ProductType>({
+  title: {
+    type: String,
+    required: true,
+    minlength: 10,
+  },
+  vendor: {
+    required: true,
+    type: Types.ObjectId,
+  },
+  postedOn: {
+    type: Date,
+    default: new Date(),
+    immutable: true,
+  },
+  rating: {
+    type: Number,
+  },
+  mfgDate: {
+    type: Date,
+    required: true,
+  },
+});
+
+const ProductModel = model<ProductType>("Product", ProductSchema);
+export { ProductModel as Product, ProductType };

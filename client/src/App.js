62b1229ad65a4a096a36474e1c9e2678ec0d25ac
Saurav Ghimire <ghimiresaurav09@gmail.com>
2022-02-23 18:27:56 +0545

    fix - user logged in bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,51 @@
-import Home from './Pages/Home';
-import ProductList from './Pages/ProductList';
-import Product from './Pages/Product';
-import Register from './Pages/Register';
-import Login from './Pages/Login';
-import Cart from './Pages/Cart';
-import Globalstyle from './globalStyles';
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./Pages/Home";
+import ProductList from "./Pages/ProductList";
+import Product from "./Pages/Product";
+import Register from "./Pages/Register";
+import Login from "./Pages/Login";
+import Cart from "./Pages/Cart";
+import Globalstyle from "./globalStyles";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
-
-const isLoggedIn = () => localStorage.getItem("name") && localStorage.getItem("userId") && localStorage.getItem("token");
+const isLoggedIn = () => {
+  if (
+    localStorage.getItem("name") &&
+    localStorage.getItem("userId") &&
+    localStorage.getItem("token")
+  )
+    return true;
+  return false;
+};
 
 function App() {
-
-  // let userLoggedIn = isLoggedIn();
-  let userLoggedIn = false;
-
   return (
     <>
-      <Globalstyle/>
+      <Globalstyle />
       <Router>
         <Routes>
-          <Route exact path="/" element={userLoggedIn ? <Home loggedIn = "true"/> : <Home loggedIn = "false"/>}/>
-          <Route path="/product-list/:category" element={userLoggedIn ? <ProductList loggedIn = "true"/> : <ProductList loggedIn = "false"/>}/>
-          <Route path="/product/:id" element={userLoggedIn ? <Product loggedIn = "true"/> : <Product loggedIn = "false"/> }/>
-          <Route path="/register" element = {userLoggedIn ? <Navigate to = "/"/> : <Register/>}/>
-          <Route path="/login" element={userLoggedIn ? <Navigate to = "/"/> :<Login/>}/>
-          <Route path="/cart" element={userLoggedIn ? <Cart loggedIn = "true"/> : <Cart loggedIn = "false"/>}/>
+          <Route exact path="/" element={<Home loggedIn={isLoggedIn()} />} />
+          <Route
+            path="/product-list/:category"
+            element={<ProductList loggedIn={isLoggedIn()} />}
+          />
+          <Route
+            path="/product/:id"
+            element={<Product loggedIn={isLoggedIn()} />}
+          />
+          <Route
+            path="/register"
+            element={isLoggedIn() ? <Navigate to="/" /> : <Register />}
+          />
+          <Route
+            path="/login"
+            element={isLoggedIn() ? <Navigate to="/" /> : <Login />}
+          />
+          <Route path="/cart" element={<Cart loggedIn={isLoggedIn()} />} />
         </Routes>
       </Router>
     </>
--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -1,214 +1,204 @@
-import styled from 'styled-components'
-import SearchIcon from '@mui/icons-material/Search';
-import MenuIcon from '@mui/icons-material/Menu';
-import PersonIcon from '@mui/icons-material/Person';
-import Badge from '@mui/material/Badge';
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-import {mobile} from '../responsive';
+import styled from "styled-components";
+import SearchIcon from "@mui/icons-material/Search";
+import MenuIcon from "@mui/icons-material/Menu";
+import PersonIcon from "@mui/icons-material/Person";
+import Badge from "@mui/material/Badge";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { mobile } from "../responsive";
 import { Link } from "react-router-dom";
-import { DropdownUser } from './Dropdown';
-import { useState } from 'react';
+import { DropdownUser } from "./Dropdown";
+import { useState } from "react";
 
 const Container = styled.div`
-    width: 100%;
-    height: 60px;
-    background-color: #000000;
-    // position: ${({scrollNav}) => (scrollNav ? 'fixed': 'relative')};
-    top: 0;
-    z-index: 10;
-    
-    ${mobile({height: "50px"})}
+  width: 100%;
+  height: 60px;
+  background-color: #000000;
+  // position: ${({ scrollNav }) => (scrollNav ? "fixed" : "relative")};
+  top: 0;
+  z-index: 10;
+
+  ${mobile({ height: "50px" })}
 `;
 
 const Wrapper = styled.div`
-    display: flex;
-    justify-content: space-between;
-    padding: 10px 80px;
-    color: #ffffff;
-    align-items: center;
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 80px;
+  color: #ffffff;
+  align-items: center;
 
-    ${mobile({padding: "10px 0"})}
+  ${mobile({ padding: "10px 0" })}
 `;
 
 const Left = styled.div`
-    flex: 1;
-    display: flex;
-    align-items: center;
+  flex: 1;
+  display: flex;
+  align-items: center;
 `;
 
 const MenuContainer = styled.div`
-    display: none;
-    ${mobile({display: "flex", margin: "0 25px", cursor: "pointer"})}
+  display: none;
+  ${mobile({ display: "flex", margin: "0 25px", cursor: "pointer" })}
 `;
 
 const SearchContainer = styled.div`
-    width: 60%;
-    border: 0.5px solid white;
-    display: flex;
-    align-items: center;
-    padding: 5px 10px;
+  width: 60%;
+  border: 0.5px solid white;
+  display: flex;
+  align-items: center;
+  padding: 5px 10px;
 
-    ${mobile({display: "none"})}
+  ${mobile({ display: "none" })}
 `;
 
 const Input = styled.input`
-    width: 100%;
-    border: none;
-    background-color: transparent;
-    color: #ffffff;
-    outline: none;
+  width: 100%;
+  border: none;
+  background-color: transparent;
+  color: #ffffff;
+  outline: none;
 
-    ::placeholder{
-        color: white;
-        opacity: 90%;
-        letter-spacing: 2px;
-}`;
+  ::placeholder {
+    color: white;
+    opacity: 90%;
+    letter-spacing: 2px;
+  }
+`;
 
 const Center = styled.div`
-    flex: 1;
-    text-align: center;
+  flex: 1;
+  text-align: center;
 `;
 
 const Logo = styled.h1`
-    font-weight: bold;
+  font-weight: bold;
 
-    ${mobile({fontSize: "24px"})}
+  ${mobile({ fontSize: "24px" })}
 `;
 
 const Right = styled.div`
-    flex: 1;
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
 `;
 
-
 const SignInContainer = styled.div`
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    &:hover{
-        color: #ded9d9;
-    }
+  &:hover {
+    color: #ded9d9;
+  }
 
-    ${mobile({display: "none"})};
+  ${mobile({ display: "none" })};
 `;
 
 const UserField = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    cursor: pointer;
-    height: 40px;
-    width: 40px;
-    padding: 9px 0;
-    border: 2px solid #ffffff;
-    border-radius: 50%;
-    position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
+  height: 40px;
+  width: 40px;
+  padding: 9px 0;
+  border: 2px solid #ffffff;
+  border-radius: 50%;
+  position: relative;
 
-    ${mobile({display: "none"})};
+  ${mobile({ display: "none" })};
 `;
 
 const UserName = styled.div`
-    // position: absolute;
-    // margin-bottom: 9px;
-    // background-color: green;
+  // position: absolute;
+  // margin-bottom: 9px;
+  // background-color: green;
 `;
 
 const SignIn = styled.a`
-    font-size: 14px;
-    margin-left: 10px;
+  font-size: 14px;
+  margin-left: 10px;
 `;
 
 const Cart = styled.div`
-    cursor: pointer;
-    margin-left: 30px;
+  cursor: pointer;
+  margin-left: 30px;
 
-    ${mobile({marginRight: "25px"})};
+  ${mobile({ marginRight: "25px" })};
 `;
 
-
 const RightJSX = (props) => {
+  //   props.loggedIn = false;
+  const getInitials = (name) => {
+    const x = name.split(" ");
+    return x[0].split("")[0] + x[x.length - 1].split("")[0];
+  };
 
-    const getInitials = (name) => {
-        const x = name.split(" ");
-        return x[0].split("")[0] + x[x.length - 1].split("")[0];
-    };
+  const [dropdownUser, setDropdownUser] = useState(false);
 
-    const [dropdownUser, setDropdownUser] = useState(false);
+  const showDropdown = () => {
+    if (window.innerWidth < 960) {
+      setDropdownUser(false);
+    } else {
+      setDropdownUser(true);
+    }
+  };
 
-    const showDropdown = () => {
-        if(window.innerWidth < 960){
-            setDropdownUser(false);
-        }
-        else{
-            setDropdownUser(true);
-        }
-    };
+  const hideDropdown = () => {
+    setDropdownUser(false);
+  };
 
-    const hideDropdown= () => {
-        setDropdownUser(false);
-    };
-
-    return props.loggedIn ? (
-
-        <UserField onMouseEnter={showDropdown} onMouseLeave={hideDropdown}>
-            <UserName>
-                {getInitials(localStorage.getItem("name"))}
-            </UserName>
-            {dropdownUser && <DropdownUser/>}
-        </UserField>
-
-    ) : (
-
-        <SignInContainer>
-            <PersonIcon/>
-            <SignIn>SIGN IN</SignIn>
-        </SignInContainer>
-      
+  return props.loggedIn ? (
+    <UserField onMouseEnter={showDropdown} onMouseLeave={hideDropdown}>
+      <UserName>{getInitials(localStorage.getItem("name"))}</UserName>
+      {dropdownUser && <DropdownUser />}
+    </UserField>
+  ) : (
+    <SignInContainer>
+      <PersonIcon />
+      <SignIn>SIGN IN</SignIn>
+    </SignInContainer>
   );
 };
 
-
 const NavBar = (props) => {
+  const linkStyle = {
+    textDecoration: "none",
+    color: "#ffffff",
+  };
 
-    const linkStyle = {
-        textDecoration: "none",
-        color: "#ffffff"
-    };
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <MenuContainer onClick={props.toggle}>
+            <MenuIcon />
+          </MenuContainer>
+          <SearchContainer>
+            <Input placeholder="Search TradeZilla..." />
+            <SearchIcon />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Link style={linkStyle} to={"/"}>
+            <Logo>TradeZilla</Logo>
+          </Link>
+        </Center>
+        <Right>
+          <RightJSX loggedIn={props.loggedIn} />
+          <Cart>
+            <Link style={linkStyle} to={"/cart"}>
+              <Badge badgeContent={2} color="primary">
+                <ShoppingCartOutlinedIcon />
+              </Badge>
+            </Link>
+          </Cart>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
 
-    return (
-        <Container>
-            <Wrapper>
-                <Left>
-                    <MenuContainer onClick = {props.toggle}>
-                        <MenuIcon/>
-                    </MenuContainer>  
-                    <SearchContainer>
-                        <Input placeholder = "Search TradeZilla..."/>
-                        <SearchIcon/>
-                    </SearchContainer>
-                </Left>
-                <Center>
-                    <Link style = {linkStyle} to = {"/"}>
-                        <Logo>
-                            TradeZilla
-                        </Logo>
-                    </Link>
-                </Center>
-                <Right>
-                    <RightJSX loggedIn={props.loggedIn} />
-                    <Cart>
-                        <Link style = {linkStyle} to = {"/cart"}>
-                            <Badge badgeContent={2} color="primary"><ShoppingCartOutlinedIcon/></Badge>
-                        </Link>        
-                    </Cart>
-                </Right>
-            </Wrapper>
-        </Container>
-    )
-}
-
-export default NavBar
+export default NavBar;

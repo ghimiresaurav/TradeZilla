4f2b2ababe3e9b11fed4838e0f9d1000ade20e89
Sajag Pradhanang <spd.sajag@gmail.com>
2022-02-24 13:37:12 +0545

    update OTP UI

--- client/src/Pages/OTP.js
+++ client/src/Pages/OTP.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import TopBars from '../Components/TopBars';
 import Footer from '../Components/Footer';
+import { useState } from 'react';
 
 const Container = styled.div`
     width: 100%;
@@ -40,10 +41,28 @@ const Label = styled.label`
     margin-bottom: 15px;
 `;
 
-const Input = styled.input`
+const InputArea = styled.div`
     height: 40px;
-    padding: 15px;
+    padding: 0 15px;
     margin-bottom: 15px;
+    display: flex;
+    justify-content: space-between;
+    // background-color: green;
+
+`;
+
+const Input = styled.input`
+    // width: 20%;
+    width: 40px;
+    // display: flex;
+    // justify-content: center;
+    // align-items: center;
+    // padding: auto;
+    // padding: 20px;
+    padding-left: 12px;
+    font-size: 20px;
+    border: 2px solid #000000;
+    border-radius: 3px;
 `;
 
 const Button = styled.button`
@@ -53,7 +72,7 @@ const Button = styled.button`
     color: #ffffff;
     background-color: #000000;
     font-size: 20px;
-    font=weight: 700;
+    font-weight: 600;
     border: 2px solid #000000;
     padding: 10px 0;
     transition: 0.3s ease-in-out;
@@ -75,25 +94,46 @@ const Resend = styled.a`
 
 
 const OTP = (props) => {
-  return (
-    <Container>
-        <TopBars loggedIn = {props.loggedIn}/>
-        <ContentArea>
-            <OTPArea>
-                <Wrapper>
-                    <Title>E-mail Verification</Title>
-                    <Label>An e-mail with a 6-digit verification code was just sent to danny@daniels.com</Label>
-                    <Input placeholder='OTP'/>
-                    <Button>Verify</Button>
-                    <Label>Didn't Receive a code?&nbsp; 
-                        <Resend>Request Again</Resend>
-                    </Label>
-                </Wrapper>
-            </OTPArea>
-        </ContentArea>
-        <Footer/>
-    </Container>
-  )
+
+    const [otp, setOtp] = useState(new Array(6).fill(""));
+
+    const handleChange = (element, index) =>{
+        if (isNaN(element.value)) return false;
+
+        setOtp([...otp.map((d, idx) => (idx === index) ? element.value : d)]);
+
+        //Focus next input
+        if (element.nextSibling){
+            element.nextSibling.focus();
+        }
+    };
+
+    return (
+        <Container>
+            <TopBars loggedIn = {props.loggedIn}/>
+            <ContentArea>
+                <OTPArea>
+                    <Wrapper>
+                        <Title>E-mail Verification</Title>
+                        <Label>An e-mail with a 6-digit verification code was just sent to danny@daniels.com</Label>
+                        <InputArea>
+                            {otp.map((data, index) => {
+                                return (
+                                    <Input type = "text" maxLength = "1" key = {index} value = {data} onChange = {e=> handleChange(e.target, index)} onFocus = {e => e.target.select()}/>
+                                )
+                            })}
+                        </InputArea>
+                        <p>OTP Entered - {otp.join("")}</p>
+                        <Button>Verify</Button>
+                        <Label>Didn't Receive a code?&nbsp; 
+                            <Resend>Request Again</Resend>
+                        </Label>
+                    </Wrapper>
+                </OTPArea>
+            </ContentArea>
+            <Footer/>
+        </Container>
+    )
 }
 
 export default OTP

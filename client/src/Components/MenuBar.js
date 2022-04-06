8bc64eb44ffcaa5dae48255e411d4beb9af771cf
Sajag Pradhanang <spd.sajag@gmail.com>
2022-04-06 16:55:59 +0545

    add links to category

--- client/src/Components/MenuBar.js
+++ client/src/Components/MenuBar.js
@@ -4,245 +4,271 @@ import CloseIcon from "@mui/icons-material/Close";
 // import {DropdownBestSellers, DropdownTodaysDeal, DropdownFashion,  DropdownElectronics, DropdownHomeKitchen, DropdownGroceries, DropdownBooks} from './Dropdown';
 import { DropdownCat } from "./Dropdown";
 import { useState } from "react";
-import {Link} from "react-router-dom";
-
+import { Link } from "react-router-dom";
 
 const ContainerMain = styled.div`
-  width: 100%;
-  background-color: #ffffff;
-  left: ${({ isOpen }) => (isOpen ? "0px" : "-300px")};
-  transition: 0.3s ease-in-out;
-  border-bottom: 2px solid #000000;
-  z-index: 100;
-  
-  ${tab({
-    overflow: "hidden",
-    overflowX: "auto", //adds scroll during overflow and removes when not needed
-    border: "none"
-  })}
+	width: 100%;
+	background-color: #ffffff;
+	left: ${({ isOpen }) => (isOpen ? "0px" : "-300px")};
+	transition: 0.3s ease-in-out;
+	border-bottom: 2px solid #000000;
+	z-index: 100;
 
-  ${mobile({
-    width: "300px",
-    height: "100vh",
-    position: "fixed",
-    top: "0",
-    overflowX: "",
-    border: "none"
-  })}
+	${tab({
+		overflow: "hidden",
+		overflowX: "auto", //adds scroll during overflow and removes when not needed
+		border: "none",
+	})}
+
+	${mobile({
+		width: "300px",
+		height: "100vh",
+		position: "fixed",
+		top: "0",
+		overflowX: "",
+		border: "none",
+	})}
 `;
 
-
 const Container = styled.div`
-  display: flex;
+	display: flex;
 
-  ${mobile({
-    display: "block",
-  })}
-  
+	${mobile({
+		display: "block",
+	})}
 `;
 
 const CloseArea = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  margin: 15px;
+	display: flex;
+	justify-content: flex-end;
+	margin: 15px;
 `;
 
 const CloseButton = styled.div`
-  display: none;
-  align-items: center;
-  justify-content: center;
-  width: 30px;
-  height: 30px;
-  border-radius: 50%;
-  transition: 0.5s;
-  color: #000000;
-  cursor: pointer;
+	display: none;
+	align-items: center;
+	justify-content: center;
+	width: 30px;
+	height: 30px;
+	border-radius: 50%;
+	transition: 0.5s;
+	color: #000000;
+	cursor: pointer;
 
-  &:hover {
-    background-color: #000000;
-    color: #ffffff;
-  }
+	&:hover {
+		background-color: #000000;
+		color: #ffffff;
+	}
 
-  ${mobile({ display: "flex" })}
+	${mobile({ display: "flex" })}
 `;
 
 const Wrapper = styled.div`
-  margin: auto;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 0px 0;
+	margin: auto;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	padding: 0px 0;
 
-  ${mobile({ display: "block", width: "80%" })}
+	${mobile({ display: "block", width: "80%" })}
 `;
 
 const MenuItem = styled.div`
-  height: 40px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  font-size: 18px;
-  font-weight: 500;
-  padding: 9px 15px;
-  cursor: pointer;
+	height: 40px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	font-size: 18px;
+	font-weight: 500;
+	padding: 9px 15px;
+	cursor: pointer;
 
-  &:hover {
-    background-color: lightgray;
-    // border-bottom: 2px solid #000000;
-  }
+	&:hover {
+		background-color: lightgray;
+		// border-bottom: 2px solid #000000;
+	}
 
-  ${mobile({ borderBottom: "1px solid gray" })}
+	${mobile({ borderBottom: "1px solid gray" })}
 `;
 
+const linkStyle = {
+	textDecoration: "none",
+	color: "#000000",
+};
+
 const MenuBar = ({ isOpen, toggle }) => {
-  //Best Sellers Dropdown
-  const [dropdownBestSellers, setDropdownBestSellers] = useState(false);
+	//Best Sellers Dropdown
+	const [dropdownBestSellers, setDropdownBestSellers] = useState(false);
 
-  const showBestSellers = () => {
-    if (window.innerWidth < 960) {
-      setDropdownBestSellers(false);
-    } else {
-      setDropdownBestSellers(true);
-    }
-  };
+	const showBestSellers = () => {
+		if (window.innerWidth < 960) {
+			setDropdownBestSellers(false);
+		} else {
+			setDropdownBestSellers(true);
+		}
+	};
 
-  const hideBestSellers = () => {
-    setDropdownBestSellers(false);
-  };
+	const hideBestSellers = () => {
+		setDropdownBestSellers(false);
+	};
 
-  //Today's Deal Dropdown
-  const [dropdownTodaysDeal, setDropdownTodaysDeal] = useState(false);
+	//Today's Deal Dropdown
+	const [dropdownSports, setDropdownSports] = useState(false);
 
-  const showTodaysDeal = () => {
-    if (window.innerWidth < 960) {
-      setDropdownTodaysDeal(false);
-    } else {
-      setDropdownTodaysDeal(true);
-    }
-  };
+	const showSports = () => {
+		if (window.innerWidth < 960) {
+			setDropdownSports(false);
+		} else {
+			setDropdownSports(true);
+		}
+	};
 
-  const hideTodaysDeal = () => {
-    setDropdownTodaysDeal(false);
-  };
+	const hideSports = () => {
+		setDropdownSports(false);
+	};
 
-  //Fashion Dropdown
-  const [dropdownFashion, setDropdownFashion] = useState(false);
+	//Fashion Dropdown
+	const [dropdownFashion, setDropdownFashion] = useState(false);
 
-  const showFashion = () => {
-    if (window.innerWidth < 960) {
-      setDropdownFashion(false);
-    } else {
-      setDropdownFashion(true);
-    }
-  };
+	const showFashion = () => {
+		if (window.innerWidth < 960) {
+			setDropdownFashion(false);
+		} else {
+			setDropdownFashion(true);
+		}
+	};
 
-  const hideFashion = () => {
-    setDropdownFashion(false);
-  };
+	const hideFashion = () => {
+		setDropdownFashion(false);
+	};
 
-  //Electronics Dropdown
-  const [dropdownElectronics, setDropdownElectronics] = useState(false);
+	//Electronics Dropdown
+	const [dropdownElectronics, setDropdownElectronics] = useState(false);
 
-  const showElectronics = () => {
-    if (window.innerWidth < 960) {
-      setDropdownElectronics(false);
-    } else {
-      setDropdownElectronics(true);
-    }
-  };
+	const showElectronics = () => {
+		if (window.innerWidth < 960) {
+			setDropdownElectronics(false);
+		} else {
+			setDropdownElectronics(true);
+		}
+	};
 
-  const hideElectronics = () => {
-    setDropdownElectronics(false);
-  };
+	const hideElectronics = () => {
+		setDropdownElectronics(false);
+	};
 
-  //Home and Kitchen Dropdown
-  const [dropdownHomeKitchen, setDropdownHomeKitchen] = useState(false);
+	//Home and Kitchen Dropdown
+	const [dropdownHouseholds, setDropdownHouseholds] = useState(false);
 
-  const showHomeKitchen = () => {
-    if (window.innerWidth < 960) {
-      setDropdownHomeKitchen(false);
-    } else {
-      setDropdownHomeKitchen(true);
-    }
-  };
+	const showHouseholds = () => {
+		if (window.innerWidth < 960) {
+			setDropdownHouseholds(false);
+		} else {
+			setDropdownHouseholds(true);
+		}
+	};
 
-  const hideHomeKitchen = () => {
-    setDropdownHomeKitchen(false);
-  };
+	const hideHouseHolds = () => {
+		setDropdownHouseholds(false);
+	};
 
-  //Groceries Dropdown
-  const [dropdownGroceries, setDropdownGroceries] = useState(false);
+	//Groceries Dropdown
+	const [dropdownGroceries, setDropdownGroceries] = useState(false);
 
-  const showGroceries = () => {
-    if (window.innerWidth < 960) {
-      setDropdownGroceries(false);
-    } else {
-      setDropdownGroceries(true);
-    }
-  };
+	const showGroceries = () => {
+		if (window.innerWidth < 960) {
+			setDropdownGroceries(false);
+		} else {
+			setDropdownGroceries(true);
+		}
+	};
 
-  const hideGroceries = () => {
-    setDropdownGroceries(false);
-  };
+	const hideGroceries = () => {
+		setDropdownGroceries(false);
+	};
 
-  //Books Dropdown
-  const [dropdownBooks, setDropdownBooks] = useState(false);
+	//Books Dropdown
+	const [dropdownBooks, setDropdownBooks] = useState(false);
 
-  const showBooks = () => {
-    if (window.innerWidth < 960) {
-      setDropdownBooks(false);
-    } else {
-      setDropdownBooks(true);
-    }
-  };
+	const showBooks = () => {
+		if (window.innerWidth < 960) {
+			setDropdownBooks(false);
+		} else {
+			setDropdownBooks(true);
+		}
+	};
 
-  const hideBooks = () => {
-    setDropdownBooks(false);
-  };
+	const hideBooks = () => {
+		setDropdownBooks(false);
+	};
 
-  return (
-    <ContainerMain isOpen={isOpen}>
-      <Container>
-        <CloseArea>
-          <CloseButton onClick={toggle}>
-            <CloseIcon />
-          </CloseButton>
-        </CloseArea>
-        <Wrapper>
-          {/* <Link to={"/product-list"} style = {{textDecoration: "none"}}> */}
-            <MenuItem onMouseEnter={showFashion} onMouseLeave={hideFashion} >
-            Fashion
-            {dropdownFashion && <DropdownCat cat="Fashion" />}
-          </MenuItem>
-          {/* </Link> */}
-          <MenuItem onMouseEnter={showTodaysDeal} onMouseLeave={hideTodaysDeal}>
-            Sports
-            {dropdownTodaysDeal && <DropdownCat cat="Sports" />}
-          </MenuItem>
-          <MenuItem onMouseEnter={showElectronics} onMouseLeave={hideElectronics}>
-            Electronics
-            {dropdownElectronics && <DropdownCat cat="Electronics" />}
-          </MenuItem>
-          <MenuItem onMouseEnter={showHomeKitchen} onMouseLeave={hideHomeKitchen}>
-            Households
-            {dropdownHomeKitchen && <DropdownCat cat="Households" />}
-          </MenuItem>
-          <MenuItem onMouseEnter={showBestSellers} onMouseLeave={hideBestSellers}>
-            Music
-            {dropdownBestSellers && <DropdownCat cat="Music" />}
-          </MenuItem>
-          <MenuItem onMouseEnter={showGroceries} onMouseLeave={hideGroceries}>
-            Groceries
-            {dropdownGroceries && <DropdownCat cat="Groceries" />}
-          </MenuItem>
-          <MenuItem onMouseEnter={showBooks} onMouseLeave={hideBooks}>
-            Books
-            {dropdownBooks && <DropdownCat cat="Books" />}
-          </MenuItem>
-        </Wrapper>
-      </Container>
-    </ContainerMain>
-  );
+	return (
+		<ContainerMain isOpen={isOpen}>
+			<Container>
+				<CloseArea>
+					<CloseButton onClick={toggle}>
+						<CloseIcon />
+					</CloseButton>
+				</CloseArea>
+				<Wrapper>
+					<Link to={"/fashion"} style={linkStyle}>
+						<MenuItem onMouseEnter={showFashion} onMouseLeave={hideFashion}>
+							Fashion
+							{dropdownFashion && <DropdownCat cat="Fashion" />}
+						</MenuItem>
+					</Link>
+					<Link to={"/sports"} style={linkStyle}>
+						<MenuItem
+							onMouseEnter={showSports}
+							onMouseLeave={hideSports}
+						>
+							Sports
+							{dropdownSports && <DropdownCat cat="Sports" />}
+						</MenuItem>
+					</Link>
+					<Link to={"/electronics"} style={linkStyle}>
+						<MenuItem
+							onMouseEnter={showElectronics}
+							onMouseLeave={hideElectronics}
+						>
+							Electronics
+							{dropdownElectronics && <DropdownCat cat="Electronics" />}
+						</MenuItem>
+					</Link>
+					<Link to={"/households"} style={linkStyle}>
+						<MenuItem
+							onMouseEnter={showHouseholds}
+							onMouseLeave={hideHouseHolds}
+						>
+							Households
+							{dropdownHouseholds && <DropdownCat cat="Households" />}
+						</MenuItem>
+					</Link>
+					<Link to={"/music"} style={linkStyle}>
+						<MenuItem
+							onMouseEnter={showBestSellers}
+							onMouseLeave={hideBestSellers}
+						>
+							Music
+							{dropdownBestSellers && <DropdownCat cat="Music" />}
+						</MenuItem>
+					</Link>
+					<Link to={"/groceries"} style={linkStyle}>
+						<MenuItem onMouseEnter={showGroceries} onMouseLeave={hideGroceries}>
+							Groceries
+							{dropdownGroceries && <DropdownCat cat="Groceries" />}
+						</MenuItem>
+					</Link>
+					<Link to={"/books"} style={linkStyle}>
+						<MenuItem onMouseEnter={showBooks} onMouseLeave={hideBooks}>
+							Books
+							{dropdownBooks && <DropdownCat cat="Books" />}
+						</MenuItem>
+					</Link>
+				</Wrapper>
+			</Container>
+		</ContainerMain>
+	);
 };
 
 export default MenuBar;

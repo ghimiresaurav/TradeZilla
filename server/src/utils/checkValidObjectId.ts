2b6ed0b0515ac22c1f36c6d309bdeaeebbff95a8
Saurav Ghimire <ghimiresaurav09@gmail.com>
2022-04-17 15:54:33 +0545

    refactor checkValidObjectId util

--- server/src/utils/checkValidObjectId.ts
+++ server/src/utils/checkValidObjectId.ts
@@ -5,7 +5,6 @@ const checkValidObjectId = (id: string) => {
   if (id.length === 24) {
     // Check if the id is valid
     if (mongoose.isValidObjectId(id)) {
-      console.log("here");
       // Invalid object ids change when converted to ObjectId at different times
       // But valid object ids do not change regardless of the times
       // So create an objectId using the value from the parameter
@@ -13,7 +12,7 @@ const checkValidObjectId = (id: string) => {
 
       // Check if the parameter is the same as the objectId that was just created
       // It has to be converted to string first, otherwise it will always return false
-      if (objectIdFromParam.toString() === id) return true;
+      return objectIdFromParam.toString() === id;
     }
   }
 

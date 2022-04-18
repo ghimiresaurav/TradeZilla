da5b5a6ded7b00976c67ad6654ecbd1718fb6501
DexterMes <kabinbhandari33@gmail.com>
2022-04-18 09:09:01 +0545

    Add produuct details feature

--- server/src/controllers/getProducts.ts
+++ server/src/controllers/getProducts.ts
@@ -2,6 +2,9 @@ import { Request, Response } from "express";
 import { Product } from "../models/Product";
 import mongoose from "mongoose";
 
+// Import function to check if the id from parameter is valid
+import checkValidObjectId from "../utils/checkValidObjectId";
+
 const getAllProducts = async (req: Request, res: Response) => {
   // Connect to the atlas database
   mongoose
@@ -46,4 +49,39 @@ const getProductsBySubCat = async (req: Request, res: Response) => {
   return res.json({ success: true, products });
 };
 
-export { getAllProducts, getProductsByCat, getProductsBySubCat };
+const getSpecificProductDetails = async (req: Request, res: Response) => {
+  // Connect to the atlas database
+  mongoose
+    .connect(<string>process.env.DB_URI)
+    .catch((e) => console.log(`Error: ${e.message}`));
+
+  // Extract product id and user id
+  const product_id = req.params.p_id;
+
+  // Check if the product_id in parameter is valid
+  // If the id is invalid, return an error message
+  if (!checkValidObjectId(product_id))
+    return res.json({
+      success: false,
+      message: "Invalid product Id",
+    });
+
+  // Get the product
+  const product = await Product.findById(product_id);
+
+  // Send false message if product not found
+  if (!product)
+    return res.json({
+      success: false,
+      message: "Product not found",
+    });
+
+  return res.json({ success: true, product });
+};
+
+export {
+  getAllProducts,
+  getProductsByCat,
+  getProductsBySubCat,
+  getSpecificProductDetails,
+};

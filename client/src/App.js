3635e60a7061d6dfd384cd5eb34ea469871981b8
Saurav Ghimire <ghimiresaurav09@gmail.com>
2022-04-18 19:03:35 +0545

    show products according to categories dynamically

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,126 @@
-import Home from './Pages/Home';
-import ProductDetail from './Pages/ProductDetail';
-import Register from './Pages/Register';
-import Login from './Pages/Login';
-import Cart from './Pages/Cart';
-import SellOnTradeZilla from './Pages/SellOnTradeZilla';
-import OTP from './Pages/OTP';
-import Globalstyle from './globalStyles';
-import UserAccount from './Pages/UserAccount';
-import PrivacyPolicy from './Pages/PrivacyPolicy';
-import Payment from './Pages/Payment';
-import Search from './Pages/Search';
-import { BrowserRouter as Router, Routes, Route, Navigate, useRoutes } from "react-router-dom";
-import Upload from './Components/Upload';
-import SubCategoriesPath from './routes/routeSubCategories';
-import Category from './Pages/Category';
+import Home from "./Pages/Home";
+import ProductDetail from "./Pages/ProductDetail";
+import Register from "./Pages/Register";
+import Login from "./Pages/Login";
+import Cart from "./Pages/Cart";
+import SellOnTradeZilla from "./Pages/SellOnTradeZilla";
+import OTP from "./Pages/OTP";
+import Globalstyle from "./globalStyles";
+import UserAccount from "./Pages/UserAccount";
+import PrivacyPolicy from "./Pages/PrivacyPolicy";
+import Payment from "./Pages/Payment";
+import Search from "./Pages/Search";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  useRoutes,
+} from "react-router-dom";
+import Upload from "./Components/Upload";
+import SubCategoriesPath from "./routes/routeSubCategories";
+import Category from "./Pages/Category";
 
-const isLoggedIn = () =>   
-   ( localStorage.getItem("name") &&
-    localStorage.getItem("userId") &&
-    localStorage.getItem("token") &&
-    localStorage.getItem("email") )
+const isLoggedIn = () =>
+  localStorage.getItem("name") &&
+  localStorage.getItem("userId") &&
+  localStorage.getItem("token") &&
+  localStorage.getItem("email");
 
-const CategoriesPath = () => useRoutes([
-  { path: "/fashion", element: <Category loggedIn={isLoggedIn()} title="Fashion"/>},
-  { path: "/sports", element: <Category loggedIn={isLoggedIn()} title="Sports"/> },
-  { path: "/electronics", element: <Category loggedIn={isLoggedIn()} title="Electronics"/> },
-  { path: "/households", element: <Category loggedIn={isLoggedIn()} title="Households"/> },
-  { path: "/music", element: <Category loggedIn={isLoggedIn()} title="Music"/> },
-  { path: "/groceries", element: <Category loggedIn={isLoggedIn()} title="Groceries"/> },
-  { path: "/books", element: <Category loggedIn={isLoggedIn()} title="Books"/> }
-]);
+const CategoriesPath = () =>
+  useRoutes([
+    {
+      path: "/:category",
+      element: <Category loggedIn={isLoggedIn()} />,
+    },
+  ]);
 
-const SearchPath = ""
+const SearchPath = "";
 function App() {
   return (
     <>
       <Globalstyle />
       <Router>
         <Routes>
-          <Route exact path="/" element={<Home loggedIn={isLoggedIn()}/>}/>
-          <Route path="/product/:id" element={<ProductDetail loggedIn={isLoggedIn()}/>}/>
-          <Route path="/register" element={isLoggedIn() ? <Navigate to="/" /> : <Register />}/>
-          <Route path="/login" element={isLoggedIn() ? <Navigate to="/" /> : <Login />}/>
-          <Route path="/cart" element={!isLoggedIn() ? <Navigate to="/"/> : <Cart loggedIn={isLoggedIn()} />}/>
-          <Route path="/useraccount" element={!isLoggedIn() ? <Navigate to="/"/> : <UserAccount loggedIn = {isLoggedIn()}/>}/>
-          <Route path="/sellontradezilla" element={!isLoggedIn() ? <Navigate to ="/"/> : <SellOnTradeZilla loggedIn = {isLoggedIn()}/>}/>
-          <Route path="/otp" element={(isLoggedIn() && !(localStorage.getItem("isActive") === "true") ? <OTP loggedIn = {isLoggedIn()}/> : <Navigate to="/" />)}/>
-          <Route exact path="/upload" element={<Upload loggedIn={isLoggedIn()}/>}/>
-          <Route path="/privacy-policy" element={<PrivacyPolicy loggedIn = {isLoggedIn()}/>} />
-          <Route path="/payment" element={!isLoggedIn() ? <Navigate to="/" /> : <Payment loggedIn = {isLoggedIn()}/>}/>
-          <Route path="/search/:product" element={<Search loggedIn = {isLoggedIn()} />}/>
+          <Route exact path="/" element={<Home loggedIn={isLoggedIn()} />} />
+          <Route
+            path="/product/:id"
+            element={<ProductDetail loggedIn={isLoggedIn()} />}
+          />
+          <Route
+            path="/register"
+            element={isLoggedIn() ? <Navigate to="/" /> : <Register />}
+          />
+          <Route
+            path="/login"
+            element={isLoggedIn() ? <Navigate to="/" /> : <Login />}
+          />
+          <Route
+            path="/cart"
+            element={
+              !isLoggedIn() ? (
+                <Navigate to="/" />
+              ) : (
+                <Cart loggedIn={isLoggedIn()} />
+              )
+            }
+          />
+          <Route
+            path="/useraccount"
+            element={
+              !isLoggedIn() ? (
+                <Navigate to="/" />
+              ) : (
+                <UserAccount loggedIn={isLoggedIn()} />
+              )
+            }
+          />
+          <Route
+            path="/sellontradezilla"
+            element={
+              !isLoggedIn() ? (
+                <Navigate to="/" />
+              ) : (
+                <SellOnTradeZilla loggedIn={isLoggedIn()} />
+              )
+            }
+          />
+          <Route
+            path="/otp"
+            element={
+              isLoggedIn() && !(localStorage.getItem("isActive") === "true") ? (
+                <OTP loggedIn={isLoggedIn()} />
+              ) : (
+                <Navigate to="/" />
+              )
+            }
+          />
+          <Route
+            exact
+            path="/upload"
+            element={<Upload loggedIn={isLoggedIn()} />}
+          />
+          <Route
+            path="/privacy-policy"
+            element={<PrivacyPolicy loggedIn={isLoggedIn()} />}
+          />
+          <Route
+            path="/payment"
+            element={
+              !isLoggedIn() ? (
+                <Navigate to="/" />
+              ) : (
+                <Payment loggedIn={isLoggedIn()} />
+              )
+            }
+          />
+          <Route
+            path="/search/:product"
+            element={<Search loggedIn={isLoggedIn()} />}
+          />
         </Routes>
-        <CategoriesPath/>
-        <SubCategoriesPath loggedIn={isLoggedIn()}/>
+        <CategoriesPath />
+        <SubCategoriesPath loggedIn={isLoggedIn()} />
       </Router>
     </>
   );
--- client/src/Pages/Category.js
+++ client/src/Pages/Category.js
@@ -4,130 +4,144 @@ import Footer from "../Components/Footer";
 import categories from "../datas/MenuItems";
 import ArrowLeftOutlinedIcon from "@mui/icons-material/ArrowLeftOutlined";
 import ArrowRightOutlinedIcon from "@mui/icons-material/ArrowRightOutlined";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MensWear } from "../datas/Products/Fashion";
 import Product from "../Components/Product";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const Container = styled.div`
-	position: absolute;
-	top: 100px;
-	width: 100%;
+  position: absolute;
+  top: 100px;
+  width: 100%;
 `;
 
 const ContentArea = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Title = styled.h1`
-	margin-top: 15px;
-	text-align: center;
-	font-size: 50px;
-	letter-spacing: 3px;
-	font-weight: 300;
+  margin-top: 15px;
+  text-align: center;
+  font-size: 50px;
+  letter-spacing: 3px;
+  font-weight: 300;
 `;
 
 const Box = styled.div`
-	position: relative;
-	height: 450px;
-	background-color: #f2f2f2;
-	width: 90%;
-	margin: 30px auto;
-	border-radius: 25px;
+  position: relative;
+  height: 450px;
+  background-color: #f2f2f2;
+  width: 90%;
+  margin: 30px auto;
+  border-radius: 25px;
 `;
 
 const SubCategoryTitle = styled.h2`
-	font-size: 30px;
-	font-weight: 400;
-	margin: 20px 0 0 20px;
+  font-size: 30px;
+  font-weight: 400;
+  margin: 20px 0 0 20px;
 `;
 
 const Items = styled.div`
-	position: relative;
-	height: 80%;
-	width: 95%;
-	margin: auto;
-	// background-color: red;
-	overflow: hidden;
+  position: relative;
+  height: 80%;
+  width: 95%;
+  margin: auto;
+  // background-color: red;
+  overflow: hidden;
 `;
 
 const Arrow = styled.div`
-	width: 50px;
-	height: 50px;
-	background-color: #fff7f7;
-	border-radius: 50%;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	position: absolute;
-	top: 0;
-	bottom: 0;
-	left: ${(props) => props.direction === "left" && "5px"};
-	right: ${(props) => props.direction === "right" && "5px"};
-	margin: auto;
-	cursor: pointer;
-	// opacity: 0.5;
-	z-index: 2;
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "5px"};
+  right: ${(props) => props.direction === "right" && "5px"};
+  margin: auto;
+  cursor: pointer;
+  // opacity: 0.5;
+  z-index: 2;
 `;
 
 const Products = styled.div`
-	display: flex;
-	transform: translateX(${(props) => props.slideIndex * -25}vw);
-	transition: all 1.5s ease;
+  display: flex;
+  transform: translateX(${(props) => props.slideIndex * -25}vw);
+  transition: all 1.5s ease;
 `;
 
 const linkStyle = {
-	textDecoration: "none",
-	color: "#000000",
+  textDecoration: "none",
+  color: "#000000",
 };
 
 const Category = (props) => {
-	document.title = props.title + " | TradeZilla";
+  const { category } = useParams();
+  // The params from the url are all lowercase
+  // To access the subcats, the category from the url has to be capitalized
+  // For eg, "fashion" has to be changed to "Fashion", "electronics" has to be changed to "Electronics"
+  const capitalizedCategory = category[0].toUpperCase() + category.substring(1);
+  document.title = capitalizedCategory + " | TradeZilla";
 
-	const [slideIndex, setSlideIndex] = useState(0);
+  const [products, setProducts] = useState([]);
 
-	const handleClick = (direction) => {
-		if (direction === "left") {
-			setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
-		} else {
-			setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
-		}
-	};
+  useEffect(async () => {
+    const resp = await fetch(`http://localhost:5000/products/${category}`);
+    const response = await resp.json();
+    if (response.success) setProducts(response.products);
+    else console.log("failed");
+  }, category);
 
-	return (
-		<Container>
-			<TopBars loggedIn={props.loggedIn} />
-			<ContentArea>
-				<Title>{props.title}</Title>
-				{categories[props.title].map((item, index) => {
-					return (
-						<Box>
-							<Link to={item.path} style={linkStyle}>
-								<SubCategoryTitle>{item.title}</SubCategoryTitle>
-							</Link>
-							<Items>
-								<Arrow direction="left" onClick={() => handleClick("left")}>
-									<ArrowLeftOutlinedIcon />
-								</Arrow>
-								<Products slideIndex={slideIndex}>
-									{MensWear.map((item) => (
-										<Product item={item} key={item.id} />
-									))}
-								</Products>
-								<Arrow direction="right" onClick={() => handleClick("right")}>
-									<ArrowRightOutlinedIcon />
-								</Arrow>
-							</Items>
-						</Box>
-					);
-				})}
-			</ContentArea>
-			<Footer />
-		</Container>
-	);
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
+
+  return (
+    <Container>
+      <TopBars loggedIn={props.loggedIn} />
+      <ContentArea>
+        <Title>{capitalizedCategory}</Title>
+        {categories[capitalizedCategory].map((item, index) => {
+          return (
+            <Box>
+              <Link to={item.path} style={linkStyle}>
+                <SubCategoryTitle>{item.title}</SubCategoryTitle>
+              </Link>
+              <Items>
+                <Arrow direction="left" onClick={() => handleClick("left")}>
+                  <ArrowLeftOutlinedIcon />
+                </Arrow>
+                <Products slideIndex={slideIndex}>
+                  {products.map((item) => (
+                    <Product item={item} key={item.id} />
+                  ))}
+                </Products>
+                <Arrow direction="right" onClick={() => handleClick("right")}>
+                  <ArrowRightOutlinedIcon />
+                </Arrow>
+              </Items>
+            </Box>
+          );
+        })}
+      </ContentArea>
+      <Footer />
+    </Container>
+  );
 };
 
 export default Category;

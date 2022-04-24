3734ee0eb14160682f65f98fdb734c0243948a1a
DexterMes <kabinbhandari33@gmail.com>
2022-04-24 12:36:38 +0545

    Add functionality order history

--- client/src/Components/UserAccount/OrderHistory.js
+++ client/src/Components/UserAccount/OrderHistory.js
@@ -1,9 +1,282 @@
-import React from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+import getThumbnailFromImage from "../../utils/getThumbnail";
+import defaultImage from "../../utils/defaultImage";
 
-const orderHistory = () => {
+const Product = styled.div`
+  width: 80vw;
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  /* padding: 30px; */
+  background-color: #f2f2f2;
+  /* background-color: red; */
+
+  border-radius: 5px;
+  /* margin: 5px 5px; */
+`;
+
+const ProductDetail = styled.div`
+  flex: 2;
+  display: flex;
+`;
+
+const Image = styled.img`
+  width: 200px;
+`;
+
+const Details = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  justify-content: space-around;
+`;
+
+const ProductName = styled.span``;
+
+const ProductId = styled.span``;
+
+const Date = styled.span``;
+
+const ProductQuantity = styled.span``;
+
+const ProductPrice = styled.div`
+  font-size: 20px;
+  font-weight: 200;
+`;
+
+const PopupHover = styled.span`
+  position: absolute;
+  background-color: #000000;
+  color: #ffffff;
+  border: 1px solid #ffffff;
+  font-size: 13px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 5px;
+  visibility: hidden;
+  bottom: -30px;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 600;
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+const RightDiv = styled.div`
+  display: flex;
+  flex: 9;
+  /* background-color: grey; */
+`;
+
+const WrapContainer = styled.div`
+  width: 100%;
+  /* background-color: red; */
+`;
+
+const InfoSection = styled.div`
+  width: 80%;
+  min-width: 500 vw;
+  margin: auto;
+  padding: 10px 0;
+  box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
+`;
+const PendingSection = styled.div``;
+const DeliveredSection = styled.div``;
+
+const SectionHeader = styled.div`
+  width: 90%;
+  margin: 10px auto;
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid #000000;
+`;
+
+const StartSection = styled.div`
+  font-size: 24px;
+  /* background-color: red; */
+`;
+
+const SectionBody = styled.div`
+  width: 90%;
+  padding: 10px;
+  margin: auto;
+  /* background-color: #c2d6d6; */
+`;
+
+const BodyWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 30px 50%;
+`;
+
+const SellOverview = () => {
+  const [pendingOrderHistories, setPendingOrderHistories] = useState([]);
+  const [deliveredOrderHistories, setDeliveredOrderHistories] = useState([]);
+  const [rejectedOrderHistories, setRejectedOrderHistories] = useState([]);
+
+  useEffect(async () => {
+    const resp = await fetch(`http://localhost:5000/s/v/get-order-history`, {
+      headers: {
+        authorization: `Bearer ${localStorage.getItem(
+          "token"
+        )} ${localStorage.getItem("userId")}`,
+      },
+    });
+    const response = await resp.json();
+    if (response.success) {
+      setDeliveredOrderHistories(response.dispatchedOrderHistories);
+      setPendingOrderHistories(response.pendingOrderHistories);
+      setRejectedOrderHistories(response.pendingOrderHistories);
+    } else console.log("failed");
+  }, []);
+
   return (
-    <div>orderHistory</div>
-  )
-}
+    <>
+      <Title>Order Histories</Title>
+      <RightDiv>
+        <WrapContainer>
+          <InfoSection>
+            <PendingSection>
+              <SectionHeader>
+                <StartSection>Pending Orders</StartSection>
+              </SectionHeader>
 
-export default orderHistory
+              <SectionBody>
+                <BodyWrapper>
+                  {pendingOrderHistories.map((item) => {
+                    return (
+                      <Product key={item._id}>
+                        <ProductDetail>
+                          <Image
+                            src={
+                              item.image
+                                ? getThumbnailFromImage(item.image)
+                                : defaultImage
+                            }
+                          />
+                          <Details>
+                            <ProductName>
+                              <b>Product:</b> {item.title}
+                            </ProductName>
+                            <ProductId>
+                              <b>ID:</b> {item._id}
+                            </ProductId>
+                            <Date>
+                              <b>Ordered Date:</b> {item.date}
+                            </Date>
+                            <ProductQuantity>
+                              <b>Quantity:</b> {item.quantity}
+                            </ProductQuantity>
+                            <ProductPrice>
+                              <b>Price: Rs</b> {item.price}
+                            </ProductPrice>
+                          </Details>
+                        </ProductDetail>
+                      </Product>
+                    );
+                  })}
+                </BodyWrapper>
+              </SectionBody>
+            </PendingSection>
+
+            <DeliveredSection>
+              <SectionHeader>
+                <StartSection>Delivered Orders</StartSection>
+              </SectionHeader>
+
+              <SectionBody>
+                <BodyWrapper>
+                  {deliveredOrderHistories.map((item) => {
+                    return (
+                      <Product key={item._id}>
+                        <ProductDetail>
+                          <Image
+                            src={
+                              item.image
+                                ? getThumbnailFromImage(item.image)
+                                : defaultImage
+                            }
+                          />
+                          <Details>
+                            <ProductName>
+                              <b>Product:</b> {item.title}
+                            </ProductName>
+                            <ProductId>
+                              <b>ID:</b> {item._id}
+                            </ProductId>
+                            <Date>
+                              <b>Ordered Date:</b> {item.date}
+                            </Date>
+                            <ProductQuantity>
+                              <b>Quantity:</b> {item.quantity}
+                            </ProductQuantity>
+                            <Date>
+                              <b>Dispatched On</b> {item.dispatchedOn}
+                            </Date>
+                          </Details>
+                        </ProductDetail>
+                      </Product>
+                    );
+                  })}
+                </BodyWrapper>
+              </SectionBody>
+            </DeliveredSection>
+
+            <DeliveredSection>
+              <SectionHeader>
+                <StartSection>Rejected Orders</StartSection>
+              </SectionHeader>
+
+              <SectionBody>
+                <BodyWrapper>
+                  {rejectedOrderHistories.map((item) => {
+                    return (
+                      <Product key={item._id}>
+                        <ProductDetail>
+                          <Image
+                            src={
+                              item.image
+                                ? getThumbnailFromImage(item.image)
+                                : defaultImage
+                            }
+                          />
+                          <Details>
+                            <ProductName>
+                              <b>Product:</b> {item.title}
+                            </ProductName>
+                            <ProductId>
+                              <b>ID:</b> {item._id}
+                            </ProductId>
+                            <Date>
+                              <b>Ordered Date:</b> {item.date}
+                            </Date>
+                            <ProductQuantity>
+                              <b>Quantity:</b> {item.quantity}
+                            </ProductQuantity>
+                            <Date>
+                              <b>Dispatched On</b> {item.dispatchedOn}
+                            </Date>
+                          </Details>
+                        </ProductDetail>
+                      </Product>
+                    );
+                  })}
+                </BodyWrapper>
+              </SectionBody>
+            </DeliveredSection>
+          </InfoSection>
+        </WrapContainer>
+      </RightDiv>
+    </>
+  );
+};
+
+export default SellOverview;
---
+++ server/src/controllers/getOrderHistory.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from "express";
+import mongoose from "mongoose";
+import { Order } from "../models/Order";
+
+const getOrderHistories = async (req: Request, res: Response) => {
+  // Connect to the atlas database
+  mongoose
+    .connect(<string>process.env.DB_URI)
+    .catch((e) => console.log(`Error: ${e.message}`));
+
+  // Extract user id
+  const user_id = new mongoose.Types.ObjectId(res.locals.id);
+
+  try {
+    // Find pending and dispatched orders separately
+    const pendingOrderHistories = await Order.find({
+      customer_id: user_id,
+      status: "pending",
+    });
+
+    const dispatchedOrderHistories = await Order.find({
+      customer_id: user_id,
+      status: "dispatched",
+    });
+
+    const rejectedOrderHistories = await Order.find({
+      customer_id: user_id,
+      status: "rejected",
+    });
+    // console.log(rejectedOrderHistories, pendingOrderHistories);
+
+    // Send a success message
+    return res.json({
+      success: true,
+      rejectedOrderHistories,
+      dispatchedOrderHistories,
+      pendingOrderHistories,
+    });
+  } catch (e: any) {
+    // If something goes wrong, send a message
+    return res.json({ success: false, message: e.message });
+  }
+};
+
+export default getOrderHistories;
